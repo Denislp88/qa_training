22cbccb737184492cd76378fcb7ecb9feda6aca8
Denislp88 <dplekomtsev@gmail.com>
2022-08-10 12:32:46 +0400

    new test

---
+++ QA-kcentr-firefox.py
@@ -0,0 +1,7 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+browser = webdriver.Firefox(executable_path="./firefox/geckodriver")
+browser.get('https://kcentr.ru/')
+print('Title: %s' % browser.title)
+browser.quit()
