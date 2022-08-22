cf4d3d742af48c8ae06d6155a66e38a3c0e6f908
Denislp88 <dplekomtsev@gmail.com>
2022-08-22 17:01:47 +0400

    5

--- QA-firefox-lekom.py
+++ QA-firefox-lekom.py
@@ -1,9 +1,16 @@
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-driver = webdriver.Firefox(executable_path="./firefox/geckodriver")
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+
+s=Service('./firefox/geckodriver')
+driver = webdriver.Firefox(service=s)
+
 driver.get('https://lekomtsev.turbo.site/')
 
 print('Title: %s' % driver.title)
+
+#element = driver.find_element(Options.XPATH,"//*[@id="1691cdb1-a2fe-4485-9589-d32ae66959a2"]/div/div/div[1]/div[3]/div/div/a/span/span")
+#element.click()
 
 driver.quit()
